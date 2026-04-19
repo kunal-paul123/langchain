cd2c628bb2ea2aa6342822dd091922715d9bbd4e
Kunal Paul <kp2346578@gmail.com>
2026-04-19 15:14:01 +0530

    done

--- 8.RAG/Document_loaders/webbase_loader.py
+++ 8.RAG/Document_loaders/webbase_loader.py
@@ -1,9 +1,32 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-url = "https://en.wikipedia.org/wiki/Cricket"
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
+
+url = "https://en.wikipedia.org/wiki/MacBook"
 
 loader = WebBaseLoader(url)
 
 docs = loader.load()
 
-print(docs[0].page_content)
+prompt = PromptTemplate(
+    template="Answer the following question \n {question} from the following text - \n {text}",
+    input_variables=["question", "text"]
+)
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({
+    "question": "when MacBook is first made?",
+    "text": docs[0].page_content
+})
+
+print(result)
+
